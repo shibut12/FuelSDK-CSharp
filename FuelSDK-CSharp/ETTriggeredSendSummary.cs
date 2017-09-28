afabe9667d5f9cbc9be3d96a0a5e3654a52645c0
sharif.ahmed <sharif.ahmed@salesforce.com>
2017-09-28 11:03:31 -0400

    trigger summary & result message

---
+++ FuelSDK-CSharp/ETTriggeredSendSummary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FuelSDK
+{
+    public class ETTriggeredSendSummary : TriggeredSendSummary
+    {
+        /// <summary>
+        /// Get this instance of ETTriggeredSendSummary.
+        /// </summary>
+        /// <returns>The <see cref="T:FuelSDK.GetReturn"/> object..</returns>
+        public GetReturn Get() { var r = new GetReturn(this); LastRequestID = r.RequestID; return r; }
+        /// <summary>
+        /// Info of this instance, ETTriggeredSendSummary.
+        /// </summary>
+        /// <returns>The <see cref="T:FuelSDK.InfoReturn"/> object..</returns>
+        public InfoReturn Info() { return new InfoReturn(this); }
+    }
+}
