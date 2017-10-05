191bab14163bdd5580c7f445564d5d7076d81cc0
sharif.ahmed <sharif.ahmed@salesforce.com>
2017-10-05 15:27:51 -0400

    loc,key

---
+++ FuelSDK-CSharp/ETPushCustomKey.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace FuelSDK
+{
+    public class ETPushCustomKey : FuelObject
+    {
+        public string ApplicationId { get; set; }
+        public CustomKey[] Keys { get; set; }
+
+        public ETPushCustomKey()
+        {
+            Endpoint = "https://www.exacttargetapis.com/push/v1/application/{ApplicationId}/key";
+            URLProperties = new[] { "ApplicationId" };
+            RequiredURLProperties = new[] { "ApplicationId" };
+        }
+
+        public ETPushCustomKey(JToken obj)
+        {
+        }
+
+        public void DeleteCustomKey()
+        {
+            if (ApplicationId == null || ApplicationId.Length == 0)
+            {
+                throw new ApplicationException("ApplicationId is either null or empty. Need to specify ApplicationId.");
+            }
+            // code to delete Custom Key & return response
+        }
+
+        public void GetCustomKey()
+        {
+            if (ApplicationId == null || ApplicationId.Length == 0)
+            {
+                throw new ApplicationException("ApplicationId is either null or empty. Need to specify ApplicationId.");
+            }
+            // code to get all Custom Keys & return response  with array of Custom Key objects
+        }
+
+        public void UpdateCustomKey()
+        {
+            if (ApplicationId == null || ApplicationId.Length == 0)
+            {
+                throw new ApplicationException("ApplicationId is either null or empty. Need to specify ApplicationId.");
+            }
+            // code to update Custom Key & return response
+        }
+
+    }
+
+    class CustomKey
+    {
+        public string Key { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FuelSDK-CSharp/ETPushLocation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace FuelSDK
+{
+    public class ETPushLocation : FuelObject
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string ProximityUuid { get; set; }
+        public int Radius { get; set; }
+        public int LocationType { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public LocationCenter Center  { get; set; }
+        public LocationAttribute[] Attributes  { get; set; }
+        public string LocationId { get; set; }
+
+        public ETPushLocation()
+        {
+            Endpoint = "https://www.exacttargetapis.com/push/v1/location";
+            URLProperties = new string[0];
+            RequiredURLProperties = new string[0];
+        }
+
+        public ETPushLocation(JToken obj)
+        {
+        }
+
+        public void CreateLocation()
+        {
+            if (Name == null || Name.Length == 0)
+            {
+                throw new ApplicationException("Name is either null or empty. Need to specify Name.");
+            }
+            // code to create location & return LocationID 
+        }
+
+        public void DeleteLocation()
+        {
+            if (LocationId == null || LocationId.Length == 0)
+            {
+                throw new ApplicationException("LocationId is either null or empty. Need to specify LocationId.");
+            }
+            Endpoint = "https://www.exacttargetapis.com/push/v1/location/{LocationId}";
+            URLProperties = new[] { "LocationId" };
+            RequiredURLProperties = new[] { "LocationId" };
+            // code to delete location & return response 
+        }
+
+        public void UpdateLocation()
+        {
+            if (LocationId == null || LocationId.Length == 0)
+            {
+                throw new ApplicationException("LocationId is either null or empty. Need to specify LocationId.");
+            }
+            if (Name == null || Name.Length == 0)
+            {
+                throw new ApplicationException("Name is either null or empty. Need to specify Name.");
+            }
+            Endpoint = "https://www.exacttargetapis.com/push/v1/location/{LocationId}";
+            URLProperties = new[] { "LocationId" };
+            RequiredURLProperties = new[] { "LocationId" };
+            // code to update location & return response 
+        }
+
+        public void GetSpecificLocation()
+        {
+            if (LocationId == null || LocationId.Length == 0)
+            {
+                throw new ApplicationException("LocationId is either null or empty. Need to specify LocationId.");
+            }
+            Endpoint = "https://www.exacttargetapis.com/push/v1/location/{LocationId}";
+            URLProperties = new[] { "LocationId" };
+            RequiredURLProperties = new[] { "LocationId" };
+            // code to get specific location & return response with Location object 
+        }
+
+        public void GetLocations()
+        {
+            // code to get all locations & return response with array of Location objects
+        }
+
+    }
+    
+    class LocationCenter
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    class LocationAttribute
+    {
+        public string Attribute { get; set; }
+        public string Value { get; set; }
+    }
+}
