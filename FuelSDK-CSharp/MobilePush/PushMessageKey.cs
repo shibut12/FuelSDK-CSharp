91c34d91d7e9b24482ee7117cbc1e6d7984262b5
Sivakumar Munuswami <smunuswami@outlook.com>
2017-10-06 15:40:03 -0400

    added pushmessagekey

---
+++ FuelSDK-CSharp/MobilePush/PushMessageKey.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FuelSDK.MobilePush
+{
+    /// <summary>
+    /// Presents a push message key class
+    /// </summary>
+    public class PushMessageKey
+    {
+        /// <summary>
+        /// Get or Set the key name
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// Get or set the value of the key
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
